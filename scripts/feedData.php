f2c1e3b14af46233f31fbc2bd5b6baf740c36a05
Myron Turner <turnermm02@shaw.ca>
2011-09-10 11:00:53 -0500

    removed duplicated domains from hrefs

--- scripts/feedData.php
+++ scripts/feedData.php
@@ -45,8 +45,10 @@ class feedData {
 		
 	}
     function description() {
+          /*
             $this->currentDataArray['item'] = 			 
 			   preg_replace('#(href|src)\s*=\s*([\'\"])/.*?/#ms', "$1=$2" . $this->news_feed_url(), $this->currentDataArray['item']);
+        */			   
 
 	  		return $this->currentDataArray['item'];
     } 
