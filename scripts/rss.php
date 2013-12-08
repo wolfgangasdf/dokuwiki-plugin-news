c9c10c600a92d1b50f4be484848ef40bbe15e366
Myron Turner <turnermm02@shaw.ca>
2013-12-08 13:44:32 -0600

    removed ttl element from channel

--- scripts/rss.php
+++ scripts/rss.php
@@ -75,8 +75,7 @@ ITEM;
 	    $link = $this->news_feed_url();
 		}
     $title = $this->channel_title();
-	$desc = $this->channel_description();
-    $ttl = $this->ttl;
+	$desc = $this->channel_description();    
 return <<<HEAD
 <?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0">
@@ -86,7 +85,6 @@ return <<<HEAD
 	<description>$desc</description>
 	<language>en-us</language>
 	<pubDate>$date</pubDate>
-	<ttl>$ttl</ttl>
 HEAD;
 
 	}
