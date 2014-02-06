23798706fa2542acb5406b283c8b4b0e81b394bd
Schplurtz le Déboulonné <schplurtz@laposte.net>
2014-02-06 02:25:17 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -4,17 +4,21 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Jacques Bouthier <jacques@jack31.net>
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
 $lang['menu']                  = 'Administration du plugin de News';
 $lang['btn_prune']             = 'Supprimer les sélections';
 $lang['btn_restore']           = 'Restaurer';
-$lang['btn_review']            = 'Reviser les suppressions';
+$lang['btn_review']            = 'Réviser les suppressions';
 $lang['btn_confirm']           = 'Confirmer les suppressions';
 $lang['invalid']               = 'Entrée invalide détectée !';
 $lang['btn_generate']          = 'Diffuser';
-$lang['instructions']          = '<p> Pour supprimer un flux, cliquer sur la case à cocher "Supprimer" en dessous du nom du flux. Ensuite cliquer &#160;
-sur "Supprimer les sélections" en haut de l\'écran. Vous pouvez supprimez plusieurs lux à la fois en une seule opération en cliquant plusieurs cases à cocher.  La table listant les flux sera mise à jour pour réfléter mais les suppressions ne seront effectives qu\'après avoir cliqué sur le bouton "Confirmer les Suppressions", moment auquel la database sera mise à jour. Cliquer sur "Restaurer" avant de confirmer remettra la table dans son état initial.</p> 
+$lang['btn_info']              = 'afficher/cacher l\'aide';
+$lang['btn_confirmfeed']       = 'Confirmer le sousflux';
+$lang['select_feed']           = 'Sélection de sousflux';
+$lang['no_permission']         = 'Vous n\'avez pas la permission de créer un nouveau flux';
+$lang['instructions']          = '<p><h3>Aide</h3>Pour supprimer un flux, cliquer sur la case à cocher "Supprimer" en dessous du nom du flux. Ensuite cliquer sur "Supprimer les sélections" en haut de l\'écran. Vous pouvez supprimez plusieurs flux à la fois en une seule opération en cochant plusieurs cases.  La table des flux sera mise à jour pour réfléter les suppressions, mais les suppressions ne seront effectives qu\'après avoir cliqué sur le bouton "Confirmer les Suppressions", moment auquel la base de données sera mise à jour. Cliquer sur "Restaurer" avant de confirmer remettra la table dans son état initial.</p> 
 <p> 
-Une suppression supprime seulement le statut actuel de la database. Vous devez également supprimer la syntaxe du plugin des pages qui &#160;
- ont généré le flux. Si l\'écriture du plugin reste sur une page, le flux de cette page sera regénéré la prochaine fois que le cache de Dokuwiki est mis à jour.&#160;  </p> &#160;&#160;   
+Une suppression affecte seulement le statut actuel de la base de données. Vous devez également supprimer les balises de flux des pages qui l\'ont généré. Si les balises de flux restent sur une page, le flux de cette page sera regénéré lors de la prochaine mise à jour du cache par DokuWiki</p>
+<p>Si votre site autorise plus d\'un flux, on y fait référence ici sous le terme de sous-flux. Pour modifier ou générer un sousflux, sélectionner le sous flux par son titre à partir du menu déroulant "Sélection de sousflux" puis xliquer sur le bouton "Confirmer le sousflux". Puis, poursuivre comme indiqué plus haut.</p>
 ';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,8 +4,13 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Jacques Bouthier <jacques@jack31.net>
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
-$lang['ttl']                   = 'Nombre de minutes après lequel un nouveau flux sera regénéré. Le défaut est 720 (12 heures)';
-$lang['title']                 = 'Nouveau Titre du Flux';
-$lang['desc']                  = 'Nouvelle description du Flux';
+$lang['ttl']                   = 'Nombre de minutes après lequel un flux de nouvelles sera regénéré. La valeur par défaut est 720 (12 heures)';
+$lang['title']                 = 'Titre du flux de nouvelles';
+$lang['desc']                  = 'Description du flux de nouvelles';
 $lang['url']                   = 'URL du site. Nécessaire si vous utilisez lib/exe/newfeed.php en ligne de commande.';
+$lang['mgr']                   = 'Nous du groupe autorisé à créer de nouveaux flux';
+$lang['adminsonly']            = 'Cocher cette case pour que seuls les administrateurs et gestionnaires puissent créer des flux.';
+$lang['chkperm']               = 'Utiliser le système de permission de l\'extesion «news». Pour les détails, voir la page de l\'extension sur https://www.dokuwiki.org/';
+$lang['createtime']            = 'Lors de la modification d\'une ntrée d\'un flux, conserver la date de création en tant que date de publication plutôt que la date de modification.';
