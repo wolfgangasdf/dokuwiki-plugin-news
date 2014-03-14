3e6ea16ded3cf6a4c4f37eea26b85cc17d797cfd
Thor Weinreich <thorweinreich@nefkom.net>
2014-03-14 01:20:26 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,4 +1,13 @@
 <?php
-$lang['ttl'] = 'Zeit in Minuten, nach der der News Feed erneut erstellt wird.  Standardwert ist 720 (=12 Stunden).';
-$lang['title'] = 'News Feed &Uuml;berschrift';
-$lang['desc'] = 'News Feed Beschreibung';
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Thor Weinreich <thorweinreich@nefkom.net>
+ */
+$lang['ttl']                   = 'Zeit in Minuten, nach der der News Feed erneut erstellt wird.  Standardwert ist 720 (=12 Stunden).';
+$lang['title']                 = 'News Feed &Uuml;berschrift';
+$lang['desc']                  = 'News Feed Beschreibung';
+$lang['url']                   = 'Seiten-URL. Wird benötigt, wenn lib/exe/newsfeed.php von der Kommandozeile aus ausgeführt wird.';
+$lang['mgr']                   = 'Name der Gruppe, die News-Feeds anlegen darf';
+$lang['adminsonly']            = 'Wähle diese Option, wenn nur Administratoren und Manager News-Feeds erstellen dürfen.';
